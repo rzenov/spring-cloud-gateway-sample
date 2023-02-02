b4f2f0bdb0070b417fe59d5c176515ff85bcdc8b
Rostislav Zenov <rostislav.zenov@intexsoft.by>
2023-02-02 15:52:28 +0300

    Init commit

---
+++ src/test/java/zenov/example/gateway/SpringCloudGatewaySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package zenov.example.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewaySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
